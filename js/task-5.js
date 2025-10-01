21cc76151389e6959f98d4459697feaf80ba0ae8
Tetiana Furmanets <tetyana.best.89@ukr.net>
2025-10-01 10:42:31 +0200

    Fix: remove duplicate getRandomHexColor function in task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -4,14 +4,6 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-
-
 const changeColorBtn = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
 
